4ad2a10ceca1d43d59396d2bfb2154952d9a96b7
Dillon <positiveprogrammer@gmail.com>
2016-06-08 15:16:11 +0100

    Added the ability to create a character with userName and gender

--- MMORPGDiscordBot/MMORPGDiscordBot.cs
+++ MMORPGDiscordBot/MMORPGDiscordBot.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace MMORPGDiscordBot
@@ -10,21 +11,46 @@ namespace MMORPGDiscordBot
     class MMORPGDiscordBot
     {
         DiscordClient bot;
-        
+        List<Player> players = new List<Player>();
         public MMORPGDiscordBot()
         {
             bot = new DiscordClient();
-            bot.Connect("changethis@email.com", "123456789");
+            bot.Connect("mmorpgdiscordbot@gmail.com", "asdf12345");
             bot.MessageReceived += BotMessageRecieved;
             bot.Wait();
         }
 
         private void BotMessageRecieved(object sender, MessageEventArgs e)
         {
-            if (e.Message.Text == "!test")
+            if (e.Message.Text == "!help")
             {
-                e.Channel.SendMessage("test message");
+                e.Channel.SendMessage("Use !Create, USERNAME, GENDER");
             }
+            else if(e.Message.Text.Contains("!Create"))
+            {
+                try
+                {
+                    string[] parms = Regex.Split(e.Message.Text.Substring(8), ",");
+                    if(parms.Length != 2)
+                    {
+                        throw new Exception();
+                    }
+                    CreateNewPlayer(e, parms);
+                }
+                catch
+                {
+                    e.Channel.SendMessage("Invalid inputs");
+                }
+                
+            }
+        }
+
+        private void CreateNewPlayer(MessageEventArgs e,String[] parms)
+        {
+            String userName = parms[0];
+            String gender = parms[1];
+            Player newPlayer = new Player(userName, gender);
+            players.Add(newPlayer);
         }
     }
 }
---
+++ MMORPGDiscordBot/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMORPGDiscordBot
+{
+    class Player
+    {
+        public String userName { get; private set; }
+        public String gender { get; private set;}
+        public Place location { get; private set; }
+        public Player(String userName, String gender)
+        {
+            this.userName = userName;
+            this.gender = gender;
+        }
+    }
+}
